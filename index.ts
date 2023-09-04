da871d020c04d4b55c9904d9c351f047b3b29c2f
Anushka7310 <anushkaguptably@gmail.com>
2023-09-04 17:17:34 +0530

    added table to print statuscode and count with name

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("anushka");
+const fs = require("fs");
+const logFilePath = "./data/prod-api-prod-out.log";
+const httpStatusCodeName = require("./http-Error-Code-Names.json");
+console.log(httpStatusCodeName);
+fs.readFile(logFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+
+  const logLines = data.split("\n");
+  const endpointCalls = {};
+  const statusCodes = {};
+  const apiCallsPerMinute = {};
+
+  logLines.forEach((line) => {
+    const match = line.match(
+      /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).*?(GET|POST|PUT|DELETE|HEAD|OPTIONS|TRACE) .*? (\d{3})\b/
+    );
+    if (match) {
+      const timestamp = match[1];
+      const statusCode = match[3];
+      const statusName = httpStatusCodeName[statusCode];
+
+      if (!statusCodes[statusName]) {
+        statusCodes[statusName] = {
+          statusCode,
+          count: 1,
+        };
+      } else {
+        statusCodes[statusName].count++;
+      }
+    }
+  });
+
+  console.table(statusCodes);
+});
